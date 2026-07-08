6ec6bce9794e38ef5664691b17f35d81f2203f54
Samuel Becker <samuel@aowis.org>
2026-07-08 07:15:54 +0200

    submodule update back to old

--- git_submodule_update.sh
+++ git_submodule_update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-git -C AOWIS-SERVER-GUI/external/AOWIS-SERVER-EPANET fetch origin main && \
-git -C AOWIS-SERVER-GUI/external/AOWIS-SERVER-EPANET switch -C main origin/main
+git submodule update --recursive --remote
+
+#git -C AOWIS-SERVER-GUI/external/AOWIS-SERVER-EPANET fetch origin main && \
+#git -C AOWIS-SERVER-GUI/external/AOWIS-SERVER-EPANET switch -C main origin/main
 
