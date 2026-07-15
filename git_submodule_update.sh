0046d0c79e9dbf822c4d2fd315dfa58f426701ff
Samuel Becker <samuel@aowis.org>
2026-07-16 01:04:45 +0200

    gui bump

--- git_submodule_update.sh
+++ git_submodule_update.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-git submodule update --recursive --remote
+#git submodule update --recursive --remote
+
+set -euo pipefail
+
+git submodule sync --recursive
+git submodule update --init --recursive --remote --jobs 8
+git submodule status --recursive
 
 #git -C AOWIS-SERVER-GUI/external/AOWIS-SERVER-EPANET fetch origin main && \
 #git -C AOWIS-SERVER-GUI/external/AOWIS-SERVER-EPANET switch -C main origin/main
