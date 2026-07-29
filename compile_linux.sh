7614be5093c7b05bc4655b320a2c795858f58c3c
Samuel Becker <samuel@aowis.org>
2026-07-29 20:46:07 +0200

    compile script with ninja

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake -S . -B build-linux
-cmake --build build-linux
+cmake -S . -B build-linux -G Ninja -DCMAKE_COLOR_DIAGNOSTICS=ON
+cmake --build build-linux --parallel
 
 ./build-linux/AOWIS-SERVER-GUI/aowis-server-gui
